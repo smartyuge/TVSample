14c996101c4116852244d0b691e96e79e9b48a33
hejunlin2013 <1007235554@qq.com>
2016-10-31 12:26:34 +0800

    1.fix some bug

--- metro/src/main/java/com/hejunlin/tvsample/widget/DrawingOrderRelativeLayout.java
+++ metro/src/main/java/com/hejunlin/tvsample/widget/DrawingOrderRelativeLayout.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 hejunlin <hejunlin2013@gmail.com>
+ * Github:https://github.com/hejunlin2013/TVSample
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hejunlin.tvsample.widget;
 
 import android.content.Context;
@@ -8,6 +23,7 @@ import android.widget.RelativeLayout;
 
 /**
  * Created by hejunlin on 2015/7/19.
+ * blog: http://blog.csdn.net/hejjunlin
  */
 public class DrawingOrderRelativeLayout extends RelativeLayout {
     private int position = 0;
--- recycleview/src/main/java/com/hejunlin/tvsample/widget/MetroViewBorderHandler.java
+++ recycleview/src/main/java/com/hejunlin/tvsample/widget/MetroViewBorderHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 hejunlin <hejunlin2013@gmail.com>
+ * Github:https://github.com/hejunlin2013/TVSample
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hejunlin.tvsample.widget;
 
 import android.animation.Animator;
@@ -19,7 +34,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+/**
+ * Created by hejunlin on 2016/9/16.
+ * blog: http://blog.csdn.net/hejjunlin
+ */
 public class MetroViewBorderHandler implements IMetroViewBorder {
 
     private String TAG = MetroViewBorderHandler.class.getSimpleName();
